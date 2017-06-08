ff68be9db67d0eb705f0c8f9cb3b3965030c5863
lovedly <1041801637@qq.com>
2017-06-09 00:04:32 +0800

    后台category模块

---
+++ tp5/1_admin_category/application/admin/controller/Index.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index()
+    {
+        return $this->fetch();    
+    }
+	
+	public function test(){
+		return '成功';
+	}
+	public function welcome(){
+	    return "欢迎来到o2o主后台首页!";
+	}
+}
